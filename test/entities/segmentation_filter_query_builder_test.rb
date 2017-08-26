37566cf15f3351de2e05d8f0969d70529a788f41
Hudolf Hess <hudolf@gmail.com>
2017-08-25 23:12:13 -0300

    Last tests

--- test/entities/segmentation_filter_query_builder_test.rb
+++ test/entities/segmentation_filter_query_builder_test.rb
@@ -55,9 +55,25 @@ class SegmentationFilterQueryBuilderTests < ActiveSupport::TestCase
         assert_equal '(_field_ <= _value_)', query_builder.get_query
     end
 
-    def get_segmentantion_filter(field, operation, value)
+    test "Should separate by AND operator when has two or more filters in the same group" do
+        filter_1 = get_segmentantion_filter('_field_', LowerOrEqualThanFilterEntity.new.get_operator_name, '_value_', 0)
+        filter_2 = get_segmentantion_filter('_field_', EqualFilterEntity.new.get_operator_name, '_value_', 0)
+        query_builder = SegmentationFilterQueryBuilder.new([filter_1, filter_2])
+        assert_equal '(_field_ <= _value_ AND _field_ = "_value_")', query_builder.get_query
+    end
+
+    test "Should separate different groups with OR operator" do
+        filter_1 = get_segmentantion_filter('_field_', EqualFilterEntity.new.get_operator_name, '_value_', 0)
+        filter_2 = get_segmentantion_filter('_field_', LowerOrEqualThanFilterEntity.new.get_operator_name, '_value_', 0)
+        filter_3 = get_segmentantion_filter('_field_', EqualFilterEntity.new.get_operator_name, '_value_', 1)
+        filter_4 = get_segmentantion_filter('_field_', LowerOrEqualThanFilterEntity.new.get_operator_name, '_value_', 1)
+        query_builder = SegmentationFilterQueryBuilder.new([filter_1, filter_2, filter_3, filter_4])
+        assert_equal '(_field_ = "_value_" AND _field_ <= _value_) OR (_field_ = "_value_" AND _field_ <= _value_)', query_builder.get_query
+    end
+
+    def get_segmentantion_filter(field, operation, value, group = 0)
         return SegmentationFilter.new({
-            'group': 0,
+            'group': group,
             'field': field,
             'operation': operation,
             'value': value
